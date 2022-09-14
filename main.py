2df1e8d2068a0ff1a3986f624a6e2c35b7da8b39
norman1208 <achmadnorman1208@gmail.com>
2022-09-14 22:39:00 +0700

    menambahkan operasi eksponensial

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-from math_function import add
+from math_function import add, exp
 
 
 def main():
-
-    data_1 = int(input("masukkan input 1 :"))
-    data_2 = int(input("masukkan input 2 :"))
     operator = input("masukkan operator :")
+    data_1 = int(input("masukkan input 1 :"))
+    if operator == "**":
+        pass
+    else:
+        data_2 = int(input("masukkan input 2 :"))
+    
 
     if operator == "+":
         result = add(data_1, data_2)
-        
-    print("{} {} {} = {} ".format(data_1, operator, data_2, result))
-
+        print("{} {} {} = {} ".format(data_1, operator, data_2, result))
+    
+    if operator == "**":
+        result = exp(data_1)
+        print("exp({})  = {} ".format(data_1, result))
 
 if __name__ == "__main__":
     print("Hello Main !")
--- math_function.py
+++ math_function.py
@@ -1,2 +1,5 @@
 def add(a, b):
     return a + b
+
+def exp(a):
+    return 2.71828**a
